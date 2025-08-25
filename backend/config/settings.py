8bfbd076200dbd30420132a85499853195e52063
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-25 20:55:56 +0700

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,5 +4,5 @@ import os
 load_dotenv()  # Load .env file
 
 MONGO_URI = os.getenv("mongodb+srv://zodiak-admin:T172L3iEbsRax2ZE@zodiak-cosmo.zjs0drb.mongodb.net/?retryWrites=true&w=majority&appName=zodiak-cosmo")
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("superrahasia123")
 ALLOWED_ORIGINS = ["http://localhost:3000", "https://your-frontend-url"]
