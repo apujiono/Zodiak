533b0c5dfabd88f69394bf7cc0e2839d71958fd8
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-25 21:48:16 +0700

    Create utils/security.py

---
+++ utils/security.py
@@ -0,0 +1,12 @@
+import bcrypt
+import jwt
+from ..config.settings import SECRET_KEY
+
+def hash_password(pw: str):
+    return bcrypt.hashpw(pw.encode(), bcrypt.gensalt())
+
+def verify_password(pw: str, hashed: str):
+    return bcrypt.checkpw(pw.encode(), hashed)
+
+def create_token(data: dict):
+    return jwt.encode(data, SECRET_KEY, algorithm="HS256")
