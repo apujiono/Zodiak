f1675045f67bd4a8d16e7bafcd97afbc37a4a30f
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-25 23:55:27 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,7 @@ import os
 
 load_dotenv()  # Load .env file
 
-MONGO_URI = os.getenv("mongodb+srv://zodiak-admin:T172L3iEbsRax2ZE@zodiak-cosmo.zjs0drb.mongodb.net/?retryWrites=true&w=majority&appName=zodiak-cosmo")
+MONGO_URI = os.getenv("MONGO_URI=mongodb+srv://apujiono11:6M4Lath4xt0qXxfI@tuyul.yb1kw0z.mongodb.net/?retryWrites=true&w=majority&appName=Tuyul
+FLASK_SECRET_KEY=umyWDQweHet4varHDoT7o9T0jrYKJhSL")
 SECRET_KEY = os.getenv("superrahasia123")
-ALLOWED_ORIGINS = ["http://localhost:3000", "https://your-frontend-url"]
+ALLOWED_ORIGINS = ["http://localhost:8000", "https://your-frontend-url"]
