a5c065ed094ae82f139ead2dc000d55a7c1b1644
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-25 21:41:00 +0700

    Delete backend/config/settings.py

--- backend/config/settings.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # Load .env file
-
-MONGO_URI = os.getenv("mongodb+srv://zodiak-admin:T172L3iEbsRax2ZE@zodiak-cosmo.zjs0drb.mongodb.net/?retryWrites=true&w=majority&appName=zodiak-cosmo")
-SECRET_KEY = os.getenv("superrahasia123")
-ALLOWED_ORIGINS = ["http://localhost:3000", "https://your-frontend-url"]
