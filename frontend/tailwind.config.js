a7adcb3d32749041565318e63dbc5da39ee499b4
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-25 20:28:08 +0700

    Create frontend/tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cosmic: '#001F3F',
+        gold: '#FFD700',
+      },
+    },
+  },
+  plugins: [],
+};
