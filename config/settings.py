fa87978f01ee941603adc58a1ef094f4b57deff1
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-26 00:11:52 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,5 +4,5 @@ import os
 load_dotenv()  # Load .env file
 
 MONGO_URI = os.getenv("mongodb+srv://apujiono11:jaA64xwDjTsIr04C@zodiak-cosmo.zjs0drb.mongodb.net/?retryWrites=true&w=majority&appName=zodiak-cosmo")
-FLASK_SECRET_KEY=umyWDQweHet4varHDoT7o9T0jrYKJhSL")
+
 SECRET_KEY = os.getenv("superrahasia123")
