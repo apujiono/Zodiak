fc22ff613432bc2c26abc06e1efe6411069abe29
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-26 00:30:52 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,4 +6,4 @@ load_dotenv()  # Load .env file
 MONGO_URI = os.getenv("mongodb+srv://apujiono11:jaA64xwDjTsIr04C@zodiak-cosmo.zjs0drb.mongodb.net/?retryWrites=true&w=majority&appName=zodiak-cosmo")
 
 SECRET_KEY = os.getenv("superrahasia123")
-ALLOWED_ORIGINS = ("https://zodiak-production.up.railway.app")
+ALLOWED_ORIGINS = ["https://zodiak-production.up.railway.app"]
