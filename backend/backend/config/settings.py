2f6193f4feb7ee970381187b0d49c7db5a177cb1
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-25 20:59:06 +0700

    Create backend/config/settings.py

---
+++ backend/backend/config/settings.py
@@ -0,0 +1,4 @@
+# Tambah MONGO_URI dari Atlas
+MONGO_URI = "mongodb+srv://zodiak-admin:T172L3iEbsRax2ZE@zodiak-cosmo.zjs0drb.mongodb.net/?retryWrites=true&w=majority&appName=zodiak-cosmo"  # Ganti dengan connection string
+SECRET_KEY = "superrahasia123"  # Random string, simpan aman
+ALLOWED_ORIGINS = ["http://localhost:3000", "https://your-frontend-url"]  # Update dengan Vercel URL
