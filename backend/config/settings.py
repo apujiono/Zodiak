e8022e4ba74d857e818b0035a2ea45f5dbadc631
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-25 20:48:13 +0700

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,3 +1,3 @@
-MONGO_URI = "your-mongo-uri"  # Ganti dengan MongoDB Atlas URI
+MONGO_URI = "mongodb+srv://zodiak-admin:T172L3iEbsRax2ZE@zodiak-cosmo.zjs0drb.mongodb.net/?retryWrites=true&w=majority&appName=zodiak-cosmo"  # Ganti dengan MongoDB Atlas URI
 SECRET_KEY = "your-secret-key"  # Ganti dengan random string
 ALLOWED_ORIGINS = ["http://localhost:3000", "https://your-frontend-url"]  # Update dengan Vercel URL
