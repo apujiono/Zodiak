f9faa45809b5c96f46a0b2772173f858d69c62ec
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-25 22:18:07 +0700

    Update logic.py

--- plugins/duel/logic.py
+++ plugins/duel/logic.py
@@ -1,7 +1,7 @@
 import random
 
 def calculate_duel(user: str, opponent: str):
-    zodiacs = {"Aries": 80, "Scorpio": 75, "Leo": 85}  # Tambah stats lain
+    zodiacs = {"Aries": 80, "Scorpio": 75, "Leo": 85}
     user_score = zodiacs.get(user.split("_")[0], 50) + random.randint(1, 20)
     opponent_score = zodiacs.get(opponent.split("_")[0], 50) + random.randint(1, 20)
     return f"{user} wins!" if user_score > opponent_score else f"{opponent} wins!"
